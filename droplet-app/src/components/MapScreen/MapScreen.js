f9112ce4f828da960493d04903d33f4e78d12894
Dennis Li <dennisli@10-248-31-21.wireless.oregonstate.edu>
2019-02-01 13:49:50 -0800

    Initial Map Test

--- droplet-app/src/components/MapScreen/MapScreen.js
+++ droplet-app/src/components/MapScreen/MapScreen.js
@@ -1,10 +1,66 @@
-import React, { Component } from 'react'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import './MapScreen.css'
 
-const MapScreen = () => (
-    <main className='map-screen'>
-        map screen here.
-    </main>
-)
+mapboxgl.accessToken = 'pk.eyJ1IjoibGlkZW5uIiwiYSI6ImNqcmg2NDU5czA4b3A0M25udmUxcWpjcmEifQ.J9ThJ9sMDK7ANhYkSpVnyg';
 
-export default MapScreen;
+class Map extends React.Component {
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      lng: 42,
+      lat: 43,
+      zoom: 1.5
+    };
+  }
+
+  componentDidMount() {
+    const { lng, lat, zoom } = this.state;
+
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: [lng, lat],
+      zoom
+    });
+
+
+    map.on('move', () => {
+      const { lng, lat } = map.getCenter();
+
+      this.setState({
+        lng: lng.toFixed(4),
+        lat: lat.toFixed(4),
+        zoom: map.getZoom().toFixed(2)
+      });
+    });
+
+    const marker = new mapboxgl.Marker()
+      .setLngLat([30.5, 50.5])
+      .addTo(map);
+    }
+
+
+
+
+  render() {
+    const { lng, lat, zoom } = this.state;
+
+    return (
+        <main ref={el => this.mapContainer = el} className="map-screen" ></main>
+
+    );
+  }
+}
+
+
+
+
+
+
+
+
+export default Map;
